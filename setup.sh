b4878715f4daba98b3ebaa8607e5fb4206b78943
Roky <scaranoantonio2004@gmail.com>
2026-02-17 18:51:20 +0000

    change 2.0

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Se hai problemi con il git da VSCode, vai sui tre pallini su VSCode
+#(a destra di View in alto a sinistra) e apri il terminale. E scrivi prima
+#"git pull origin main --rebase" e poi "git push origin main" e stai apposto.
+#Tutto questo nel terminale di VSCode
+
 echo "--- STARTING AUTOMATION ---"
 
 #Update package repositories
